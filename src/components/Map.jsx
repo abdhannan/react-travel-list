2c5715de130a29bf3d68deb1b989d379d06a80a5
Abd Hannan Air <apple@apples-MacBook-Air.local>
2024-07-30 03:17:20 +0700

    add reusable component, add Navigate component for redirect and more

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,32 @@
 import styles from './Map.module.css';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const navigate = useNavigate();
+
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+      <h1>Map</h1>
+      <p>
+        Position {lat}, {lng}{' '}
+      </p>
+      <button
+        onClick={() => {
+          setSearchParams({
+            lat: 10,
+            lng: 20790,
+          });
+        }}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
